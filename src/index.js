3d6f1af794ff45e8a1b841e26ec590ea27158bec
szzend <szzend@qq.com>
2018-04-18 17:32:58 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-//import {fn} from "./cmt"
-fn=require("./cmt")
-console.log(fn.fn(3))
+import Icon from './imgs/icon.jpg';
+import './css/style.css'
+function component() {
+    let fn=a=>a**2;
+    var element = document.createElement('div');
+
+    console.log(fn(3))
+    element.innerHTML = "webpack demo.";
+    element.classList.add('hello');
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    return element;
+  }
+
+  document.body.appendChild(component());
